c2cc3a44671dd4b3bd46f4f05a417778a85f8510
PaumdeBanana <119055746+PaumdeBanana@users.noreply.github.com>
2022-12-22 17:45:35 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+const pegarObjeto = (id, mensagemDeErro) => {
+    let objeto = document.getElementById(id);
+    if (objeto === null) throw mensagemDeErro;
+    return objeto;
+}
+
+const form = document.getElementById('form')
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    console.log('Deu certo')
+})
+
+let cursos = ['InformÃ¡tica', 'EdificaÃ§Ãµes', 'MecÃ¢nica'];
+let mensagens = ['', 'your lindor', 'your pedreiro', 'your cringe']
+
+let addMensagem = () => {
+    let botao = pegarObjeto('btn', 'Elemento botÃ£o nÃ£o encontrado!');
+    let mensagem = pegarObjeto('texto', 'Elemento texto nÃ£o encontrado!');
+
+    botao.addEventListener('click', () => {
+        var select = document.querySelector(".form-select");
+        for (let i = 0; i < select.options.length; i++) {
+            if (select.selectedIndex == i && select.selectedIndex != 0) {
+                document.querySelector('.caixa-de-mensagem').style.display = 'flex';
+                mensagem.innerHTML = mensagens[i];
+            }
+
+        }
+    })
+
+}
+
+addMensagem();
